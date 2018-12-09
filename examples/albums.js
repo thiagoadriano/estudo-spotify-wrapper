dc589b3087e0c38506c5207fef44fb0de85ba9a7
Thiago Adriano <thiago.s.adriano@gmail.com>
2018-12-09 11:17:35 -0200

    adicionado arquivos do projeto

---
+++ examples/albums.js
@@ -0,0 +1,9 @@
+/* to run: babel-node albums.js */
+
+global.fetch = require('node-fetch');
+
+import { searchAlbums } from '../src/main';
+
+const albums = searchAlbums('Incubus');
+
+albums.then(data => data.albums.items.map(item => console.log(item.name)));
